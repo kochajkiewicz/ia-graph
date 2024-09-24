53b07f90ff54515687096b373a00735563003c0a
Igor Kochajkiewicz <igor@nory.ai>
2024-09-24 21:43:49 +0100

    more console logs, delete docs again

--- src/App.js
+++ src/App.js
@@ -149,7 +149,8 @@ function App() {
 
     // Create a sphere geometry for the node
     const sphereGeometry = new THREE.SphereGeometry(size); // use size calculated above
-    console.log(size);
+    console.log("Size:" + size);
+    console.log("Node degree:" + nodeDegree);
     const sphereMaterial = new THREE.MeshBasicMaterial({
       color: new THREE.Color(colorScale(nodeDegree)) // Apply gradient color based on degree
     });
